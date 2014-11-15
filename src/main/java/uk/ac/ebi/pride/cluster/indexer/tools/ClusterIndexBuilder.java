56f3fe025d4264c5f9a3b6ecdd4216f9c27eb3a2
Noemi del Toro Ayllón <noedelta@gmail.com>
2014-11-15 18:56:14 +0000

    Adds species to the indexer

--- src/main/java/uk/ac/ebi/pride/cluster/indexer/tools/ClusterIndexBuilder.java
+++ src/main/java/uk/ac/ebi/pride/cluster/indexer/tools/ClusterIndexBuilder.java
@@ -1,14 +1,14 @@
 package uk.ac.ebi.pride.cluster.indexer.tools;
 
-import org.apache.solr.client.solrj.SolrServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 import uk.ac.ebi.pride.archive.ontology.search.service.OntologyTermSearchService;
 import uk.ac.ebi.pride.cluster.indexer.ClusterIndexer;
-import uk.ac.ebi.pride.cluster.search.service.ClusterIndexService;
-import uk.ac.ebi.pride.cluster.search.service.ClusterSearchService;
+import uk.ac.ebi.pride.cluster.search.service.IClusterIndexService;
+import uk.ac.ebi.pride.cluster.search.service.IClusterSearchService;
+import uk.ac.ebi.pride.spectracluster.repo.dao.IClusterReadDao;
 
 /**
  * @author ntoro
@@ -24,21 +24,13 @@ public class ClusterIndexBuilder {
     private OntologyTermSearchService ontologyTermSearchService;
 
     @Autowired
-    private ClusterIndexService clusterIndexService;
+    private IClusterIndexService clusterIndexService;
 
     @Autowired
-    private ClusterSearchService clusterSearchService;
-
-    /*
-    HttpSolrServer is thread-safe and if you are using the following constructor,
-    you *MUST* re-use the same instance for all requests.  If instances are created on
-    the fly, it can cause a connection leak. The recommended practice is to keep a
-    static instance of HttpSolrServer per solr server url and share it for all requests.
-    See https://issues.apache.org/jira/browse/SOLR-861 for more details
-    */
+    private IClusterSearchService clusterSearchService;
 
     @Autowired
-    private SolrServer solrClusterServer;
+    private IClusterReadDao clusterReadDao;
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring/app-context.xml");
@@ -46,13 +38,37 @@ public class ClusterIndexBuilder {
         ClusterIndexBuilder clusterIndexBuilder = context.getBean(ClusterIndexBuilder.class);
         indexClusters(clusterIndexBuilder);
 
+        if (args.length>0 && "inc".equals(args[0].toLowerCase())) {
+            indexNonExistingClusters(clusterIndexBuilder);
+        } else if (args.length>0 && "all".equals(args[0].toLowerCase())) {
+            indexClusters(clusterIndexBuilder);
+        } else {
+            System.out.println("Arguments:");
+            System.out.println("   inc   - index cluster not already in the index");
+            System.out.println("   all   - deletes the index and index all clusters");
+        }
+
+    }
+
+    private static void indexNonExistingClusters(ClusterIndexBuilder clusterIndexBuilder) {
+
+        ClusterIndexer clusterIndexer = new ClusterIndexer(
+                clusterIndexBuilder.clusterSearchService,
+                clusterIndexBuilder.clusterIndexService,
+                clusterIndexBuilder.clusterReadDao,
+                clusterIndexBuilder.ontologyTermSearchService
+        );
+
+        clusterIndexer.indexNonExistingClusters();
     }
 
     public static void indexClusters(ClusterIndexBuilder clusterIndexBuilder) {
 
         ClusterIndexer clusterIndexer = new ClusterIndexer(
                 clusterIndexBuilder.clusterSearchService,
-                clusterIndexBuilder.clusterIndexService
+                clusterIndexBuilder.clusterIndexService,
+                clusterIndexBuilder.clusterReadDao,
+                clusterIndexBuilder.ontologyTermSearchService
         );
 
         clusterIndexer.indexAllClusters();
